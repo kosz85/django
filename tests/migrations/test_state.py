40a5e29ec4827a08f3f934c5ff2db0a03a2fe5ce
Konrad Szymański <ko.szymanski@gmail.com>
2015-09-18 09:24:11 +0200

    Changed inheritance order to more correct, added attr and test and fixed typo

--- tests/migrations/test_state.py
+++ tests/migrations/test_state.py
@@ -805,20 +805,20 @@ class ModelStateTests(TestCase):
         new_apps = Apps(['migrations'])
 
         class DummyMetaClass(type):
+            dummy_attr = 'dummy'
             pass
 
         class Mixin(six.with_metaclass(DummyMetaClass, object)):
-            mixin_attr = True
+            mixin_attr = 'mixin'
 
-        class ItermidiateMetaClass(ModelBase, DummyMetaClass):
+        class IntermediateMetaClass(DummyMetaClass, ModelBase):
             pass
 
         # Create the same Model without metaclassmaker as you would normally do.
-        class MetaclassTestModel(six.with_metaclass(ItermidiateMetaClass, models.Model, Mixin)):
+        class MetaclassTestModel(six.with_metaclass(IntermediateMetaClass, Mixin, models.Model)):
             class Meta:
                 app_label = "migrations"
                 apps = new_apps
-                swappable = 'TEST_SWAPPABLE_MODEL'
 
         # Reset apps to be able to render __fake__ modules.
         new_apps = Apps(['migrations'])
@@ -827,8 +827,8 @@ class ModelStateTests(TestCase):
         # This raises metaclass conflict if it's not done properly.
         class_obj = model_state.render(new_apps)
 
-        self.assertEqual(getattr(class_obj, 'mixin_attr', False), True)
-
+        self.assertEqual(getattr(class_obj, 'mixin_attr', False), 'mixin')
+        self.assertEqual(getattr(class_obj, 'dummy_attr', False), 'dummy')
 
 class RelatedModelsTests(SimpleTestCase):
 
